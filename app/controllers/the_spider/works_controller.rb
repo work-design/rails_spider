43c8283e9eaf270fb407eb9e6f82d0734570851b
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-04 22:20:53 +0800

    update index

--- app/controllers/the_spider/works_controller.rb
+++ app/controllers/the_spider/works_controller.rb
@@ -1,28 +1,22 @@
-require_dependency "the_spider/application_controller"
-
+require_dependency 'the_spider/application_controller'
 module TheSpider
   class WorksController < ApplicationController
     before_action :set_work, only: [:show, :edit, :update, :destroy]
 
-    # GET /works
     def index
       @works = Work.all
     end
 
-    # GET /works/1
     def show
     end
 
-    # GET /works/new
     def new
       @work = Work.new
     end
 
-    # GET /works/1/edit
     def edit
     end
 
-    # POST /works
     def create
       @work = Work.new(work_params)
 
@@ -33,7 +27,6 @@ module TheSpider
       end
     end
 
-    # PATCH/PUT /works/1
     def update
       if @work.update(work_params)
         redirect_to @work, notice: 'Work was successfully updated.'
@@ -42,21 +35,18 @@ module TheSpider
       end
     end
 
-    # DELETE /works/1
     def destroy
       @work.destroy
       redirect_to works_url, notice: 'Work was successfully destroyed.'
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_work
-        @work = Work.find(params[:id])
-      end
+    def set_work
+      @work = Work.find(params[:id])
+    end
 
-      # Only allow a trusted parameter "white list" through.
-      def work_params
-        params.fetch(:work, {})
-      end
+    def work_params
+      params.fetch(:work, {})
+    end
   end
 end
