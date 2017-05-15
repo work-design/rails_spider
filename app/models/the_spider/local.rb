5a4738594bf97e845549f230fdff6b12bc1c77ef
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-15 11:00:23 +0800

    to use config parser

--- app/models/the_spider/local.rb
+++ app/models/the_spider/local.rb
@@ -1,10 +1,9 @@
 module TheSpider
   class Local < ApplicationRecord
     belongs_to :work
-
-
+    
     def parser
-      @parser ||= Parser.new(self.body)
+      @parser ||= work.parser.new(self.body)
     end
 
     def run
