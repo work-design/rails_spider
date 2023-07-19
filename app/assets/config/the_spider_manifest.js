caed6d5bd5d71f163f0da0d619e3406d9274039a
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-19 20:53:54 +0800

    simplefile

--- app/assets/config/the_spider_manifest.js
+++
@@ -1,4 +0,0 @@
-//= link_directory ../javascripts/rails_spider .js
-//= link_directory ../stylesheets/rails_spider .css
-//= link rails_spider/application.js
-//= link rails_spider/application.css
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-
-  scope :admin, module: 'spider/admin', as: :admin do
-    resources :spider_works, shallow: true do
-      get 'add_item/:item' => :add_item, on: :collection, as: :add_item
-      get 'remove_item/:item' => :remove_item, on: :collection, as: :remove_item
-      patch :run, on: :member
-      resources :spider_caches
-      resources :spider_resources
+  namespace :spider do
+    namespace :admin do
+      resources :spider_works, shallow: true do
+        get 'add_item/:item' => :add_item, on: :collection, as: :add_item
+        get 'remove_item/:item' => :remove_item, on: :collection, as: :remove_item
+        patch :run, on: :member
+        resources :spider_caches
+        resources :spider_resources
+      end
     end
   end
 
